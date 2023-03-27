5e7a19ac0776d87f47cce1a3976c7deb52b3492b
Aayush2104 <aayush.2104@gmail.com>
2023-03-27 18:52:30 +0530

    changed folder structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import Login from "./pages/login";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/home";
 import AddProduct from "./pages/addProduct";
+import Productlist from "./pages/productlist"
 
 
 function App() {
@@ -12,6 +13,8 @@ function App() {
         <Route exact path="/" element={<Login/>} />
         <Route path="/home" element={<Home/>} />
         <Route path="/addProduct" element={<AddProduct/>} />
+        <Route path="/productList" element={<Productlist/>} />
+        
       </Routes>
     </>
   );
